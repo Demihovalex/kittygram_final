265473cd948113ea070695ed88767f89f63ee2fc
Alexander Demihov <demihov.alex@yandex.ru>
2026-05-10 12:23:45 +0300

    Fix flake8 errors in urls.py

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -4,21 +4,26 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
+
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
+
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
         'cats': reverse('cat-list', request=request, format=format),
-        'achievements': reverse('achievement-list', request=request, format=format),
+        'achievements': reverse(
+            'achievement-list', request=request, format=format
+        ),
         'users': reverse('user-list', request=request, format=format),
     })
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),

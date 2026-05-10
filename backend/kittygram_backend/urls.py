2f70a0c62d745d9f57893baa99343761018163e8
Alexander Demihov <demihov.alex@yandex.ru>
2026-05-10 12:20:38 +0300

    Add root API endpoint to fix 500 error

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -4,16 +4,28 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'cats': reverse('cat-list', request=request, format=format),
+        'achievements': reverse('achievement-list', request=request, format=format),
+        'users': reverse('user-list', request=request, format=format),
+    })
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api_root, name='api-root'),
     path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),  # Работа с пользователями
-    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
 ]
 
 if settings.DEBUG:
